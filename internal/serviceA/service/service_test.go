4393080b32fb53124c0ceb2e2ece7dcfe5237c92
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2022-07-18 12:00:01 -0300

    Added When spec to service tests

--- internal/serviceA/service/service_test.go
+++ internal/serviceA/service/service_test.go
@@ -20,45 +20,56 @@ var _ = Describe("Service A", func() {
 	s := service.New(logMock)
 
 	Context("Testing CRUD Operations", func() {
-		Context("When user looks for all items", func() {
-			expectedItems := assertion.ItemArray
-			It("Should return all items from DB", func() {
-				resp, err := s.GetAll()
-				Expect(err).ShouldNot(HaveOccurred())
-				Expect(resp).Should(Equal(expectedItems))
+
+		Context("Getting All items", func() {
+			When("Request succeeds", func() {
+				expectedItems := assertion.ItemArray
+				It("Should return all items from DB", func() {
+					resp, err := s.GetAll()
+					Expect(err).ShouldNot(HaveOccurred())
+					Expect(resp).To(Equal(expectedItems))
+				})
 			})
 		})
 
-		Context("When user looks for an specific item", func() {
-			expectedItem := assertion.NewItemWithID(assertion.SampleID.String())
-			It("Should return an item with given ID", func() {
-				resp, err := s.GetOneByID(expectedItem.ID)
-				Expect(err).ShouldNot(HaveOccurred())
-				Expect(resp).Should(Equal(expectedItem))
+		Context("Getting one item by ID", func() {
+			When("Request succeeds", func() {
+				expectedItem := assertion.NewItemWithID(assertion.SampleID.String())
+				It("Should return an item with given ID", func() {
+					resp, err := s.GetOneByID(expectedItem.ID)
+					Expect(err).ShouldNot(HaveOccurred())
+					Expect(resp).To(Equal(expectedItem))
+				})
 			})
 		})
 
-		Context("When user creates an item", func() {
-			itemInput := assertion.NewItemWithoutID()
-			It("Should return the created object", func() {
-				resp, err := s.Create(itemInput)
-				Expect(err).ShouldNot(HaveOccurred())
-				Expect(resp.ID).ShouldNot(BeNil())
+		Context("Creating an item", func() {
+			When("Request succeeds", func() {
+				itemInput := assertion.NewItemWithoutID()
+				It("Should return the created object", func() {
+					resp, err := s.Create(itemInput)
+					Expect(err).ShouldNot(HaveOccurred())
+					Expect(resp.ID).NotTo(BeNil())
+				})
 			})
 		})
 
-		Context("When user updates an item", func() {
-			inputItem := assertion.NewItemWithID(assertion.SampleID.String())
-			It("Should return nothing", func() {
-				err := s.Update(assertion.SampleID, inputItem)
-				Expect(err).ShouldNot(HaveOccurred())
+		Context("Updating an item", func() {
+			When("Request succeeds", func() {
+				inputItem := assertion.NewItemWithID(assertion.SampleID.String())
+				It("Should return nothing", func() {
+					err := s.Update(assertion.SampleID, inputItem)
+					Expect(err).ShouldNot(HaveOccurred())
+				})
 			})
 		})
 
-		Context("When user deletes an item", func() {
-			It("Should return nothing", func() {
-				err := s.Delete(assertion.SampleID)
-				Expect(err).ShouldNot(HaveOccurred())
+		Context("Deleting an item", func() {
+			When("Request succeeds", func() {
+				It("Should return nothing", func() {
+					err := s.Delete(assertion.SampleID)
+					Expect(err).ShouldNot(HaveOccurred())
+				})
 			})
 		})
 	})
