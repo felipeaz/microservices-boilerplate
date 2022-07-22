ac6dc299ec521c96c7991fad682399c31ddb4cd5
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2022-07-22 14:59:44 -0300

    Added initial structure for repository tests

---
+++ internal/serviceB/repository/repository_test.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestService(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Repository Suits")
+}
+
+var _ = Describe("Repository", func() {
+	Context("Getting all items", func() {
+		Context("Items are in cache", func() {
+
+		})
+
+		Context("Items are not in cache", func() {
+
+		})
+	})
+
+	Context("Getting one item", func() {
+		Context("Item is in cache", func() {
+
+		})
+
+		Context("Item is not in cache", func() {
+
+		})
+	})
+
+	Context("Creating an item", func() {
+
+	})
+
+	Context("Updating an item", func() {
+
+	})
+
+	Context("Deleting an item", func() {
+
+	})
+})
